ed560e2c02dada27fb8a82ed2159ba694a54d785
abdalsalam safadi <133901135+matrix-911@users.noreply.github.com>
2025-05-09 22:35:35 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask
-from flask_cors import CORS  # ✅ Add this line
+from app.api import api_bp
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # ✅ Enable CORS for all domains
-
-    from .api import api_bp
-    app.register_blueprint(api_bp)
-    
+    app.register_blueprint(api_bp, url_prefix='/api')
     return app
