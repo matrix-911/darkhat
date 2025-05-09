bd355c0e658fcdfe5838873ce25eb7e236945ca9
abdalsalam safadi <133901135+matrix-911@users.noreply.github.com>
2025-05-09 23:12:57 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
-from app.api import api_bp
+from flask_cors import CORS  # ✅ import CORS
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(api_bp, url_prefix='/api')
+
+    CORS(app)  # ✅ ENABLE CORS for external calls like from Wix
+
+    from .api import api_bp
+    app.register_blueprint(api_bp, url_prefix="/api")
+
     return app
